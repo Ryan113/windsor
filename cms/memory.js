8a21d94af7ff8c50109a524fbdba65229e48cbcd
Ryan <>
2018-12-28 10:52:30 -0800

    removed example page

--- cms/memory.js
+++ cms/memory.js
@@ -1,34 +1,15 @@
 ({
 	header: {
-		header: 'Windsor Gardens',
+		color: '255, 248, 224',
+		header: 'Memory Care',
 		tagline: 'Welcome to Windsor Gardens! We provide memory care and assisted living services for seniors in Vancouver, Wa',
 		$background_image_type: 'image',
 		$background_image_label: 'Background Image',
 		background_image: '/remote/direct_uploads/1545966687_stock-photo-happy-patient-is-holding-caregiver-for-a-hand-while-spending-time-together-688645165.jpg'
 	},
 	information: {
-		header: 'Memory Care for Seniors',
+		header: 'Memory Care',
 		body: 'Welcome to Windsor Gardens! We provide memory care and assisted living services for seniors in Vancouver, Washington. We help seniors deal with memory loss such as Dementia and Alzheimer’s.  The decision to admit someone in need of memory care to a new environment can be very difficult. We understand the challenges can be very difficult. When you choose us you will appreciate our dedication to providing individualized care with dignity.  At Windsor Gardens, the warm elegant environment will invite you in. Windsor Gardens is a secure gated environment, ensuring the safety of your loved one. From the dedicated staff to the wonderful atmosphere, it is a truly great place to call home. Become a part of our assisted living community in Vancouver, WA.  Many Alzheimer and dementia patients have the need to walk and keep moving. You will find that the tranquil setting at Windsor Gardens brings comfort. Our one-acre park like setting invites you to enjoy the beautiful walking paths and peaceful sitting areas.'
-	},
-	types_of_care: {
-		list_of_care_types: [
-			{
-				name: 'Memory Care',
-				info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pretium egestas diam. Morbi viverra nunc id nisi dignissim mollis.'
-			},
-			{
-				name: 'Assisted Living',
-				info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pretium egestas diam. Morbi viverra nunc id nisi dignissim mollis.'
-			},
-			{
-				name: 'Respite Care',
-				info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pretium egestas diam. Morbi viverra nunc id nisi dignissim mollis.'
-			},
-			{
-				name: 'Other Care',
-				info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pretium egestas diam. Morbi viverra nunc id nisi dignissim mollis.'
-			}
-		]
 	},
 	types_of_block: {
 		individual_block: [
@@ -37,6 +18,14 @@
 				header: 'Schedule A Tour',
 				body: 'We understand that everyone has special needs. Call today for a free care assessment and see for yourself. Our admissions specialist will sit with you and customize our variety of assisted living and memory care services to your individual circumstances. Your schedule will be accommodated anytime.'
 			}
+		],
+		image_block: [
+			{
+				color: '255, 248, 224',
+				header: 'Schedule A Tour',
+				body: 'We understand that everyone has special needs. Call today for a free care assessment and see for yourself. Our admissions specialist will sit with you and customize our variety of assisted living and memory care services to your individual circumstances. Your schedule will be accommodated anytime.',
+        image: '/remote/direct_uploads/1545966687_stock-photo-happy-patient-is-holding-caregiver-for-a-hand-while-spending-time-together-688645165.jpg'
+      }
 		]
 	},
 	footer_info: {
